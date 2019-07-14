fd82b571efe1c58daeb12c690a6fcebc8efde0f4
Daniel Törbacka <daniel.torbacka@gmail.com>
2019-07-14 06:03:38 +0200

    Remove some debug logging

--- backend/service/client/SystembolagetClient.py
+++ backend/service/client/SystembolagetClient.py
@@ -7,7 +7,5 @@ class SystembolagetClient:
     _session = requests.Session()
 
     def get_articles(self):
-        print("hello")
         xml = self._session.get(self._url).content
-        print("hello")
         return untangle.parse(str(xml, "utf-8"))
